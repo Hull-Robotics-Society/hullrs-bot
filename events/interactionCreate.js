9e1a007ca251723b26d0d5f96d62c19248f7ea66
KieranRobson <kieranrobson1999@gmail.com>
2022-06-27 15:31:44 +0100

    RM defer reply from interactionCreate

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,7 +4,6 @@ const client = require("../index");
 client.on("interactionCreate", async (interaction) => {
     // Slash Command Handling
     if (interaction.isCommand()) {
-        await interaction.deferReply({ ephemeral: false }).catch(() => {});
 
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd)
@@ -28,8 +27,6 @@ client.on("interactionCreate", async (interaction) => {
     // Context Menu Handling
     if (interaction.isContextMenu()) 
     {
-
-
     }
 
     if (interaction.isButton()) 
