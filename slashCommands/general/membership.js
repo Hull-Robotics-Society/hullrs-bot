7b8e043f3255b77511c7a7e53859518bcc06f083
Kieran <kieranrobson1999@gmail.com>
2022-09-01 23:43:50 +0100

    Added membership slash command

---
+++ slashCommands/general/membership.js
@@ -0,0 +1,22 @@
+
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "membership",
+    description: "returns membership purchase link",
+    type: 'CHAT_INPUT',
+    /**
+     *
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (client, interaction, args) => {
+        const embed = new MessageEmbed()
+        .setTitle("Thanks for considering to become a paid member!")
+        .setColor('#31429a')
+        .setFooter({ text: `Called By: ${interaction.user.tag}`})
+        .addField('You can purchase from below!','https://hulluniunion.com/shop?aid=335')
+        interaction.reply({ embeds: [embed] });
+    },
+};
