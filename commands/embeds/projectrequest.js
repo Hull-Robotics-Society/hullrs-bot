7097a974d36c38e121acb7857e2fd3a82db22a85
Kieran <kieranrobson1999@gmail.com>
2022-09-06 11:43:44 +0100

    Added project requestembed

---
+++ commands/embeds/projectrequest.js
@@ -0,0 +1,39 @@
+const { MessageEmbed, MessageActionRow, MessageButton} = require("discord.js");
+
+module.exports = {
+    name: "projectembed",
+    aliases: [''],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+
+    run: async (client, message, args) => {
+        const embed = new MessageEmbed()
+        .setTitle("Project Request!")
+        .setDescription(`Have an awesome project idea? Fill in the form below to pitch it to the execs!`)
+        .setColor('#31429a')
+        .setFooter(`Called By: ${message.author.tag}`)
+        .setTimestamp()
+
+
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setCustomId('currentprojects')
+            .setLabel('See Current Projects')
+            .setStyle('PRIMARY')
+        )
+        .addComponents(
+            new MessageButton()
+            .setCustomId('projectSuggest')
+            .setLabel('Suggest a Project')
+            .setStyle('SUCCESS')
+        )
+
+        message.channel.send({ embeds: [embed], components: [row] })
+    }
+}
+
