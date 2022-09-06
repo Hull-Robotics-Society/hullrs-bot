14d482225c17355fef530b121cd1ff75eb74fa2d
Kieran <kieranrobson1999@gmail.com>
2022-09-06 18:32:38 +0100

    Add paid member embed

---
+++ commands/embeds/paidmember.js
@@ -0,0 +1,42 @@
+const {MessageEmbed, MessageActionRow, MessageButton, Permissions } = require("discord.js");
+
+module.exports = {
+    name: "paidmember",
+    aliases: [''],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+
+        if(!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS, Permissions.FLAGS.ADMINISTRATOR)) return message.channel.send("You don't have permission to use that command.")
+
+        const embed = new MessageEmbed()
+            .setColor('#31429a')
+            .setFooter({ text: `Called By: ${message.author.tag}`}) 
+            .setTimestamp()
+            .setTitle("Paid Member!")
+            .setDescription(`Press the button below to get access to the paid member role.`)
+            .addField('Benefits?', 'Come to every event\r\n Vote in AGMs and EGMs\r\n Access to the rest of the server\r\n\r\nAnd support the society you are apart of!')
+            .addField('Pay for a membership', 'Press the grey button below!')
+
+
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setURL('https://hulluniunion.com/shop?aid=355')
+                .setEmoji('')
+                .setLabel('Pay for a membership!')
+                .setStyle('LINK')
+            )
+            .addComponents(
+                new MessageButton()
+                .setCustomId('paidModal')
+                .setLabel('Paid Member Request')
+                .setStyle('PRIMARY')
+            )
+        message.channel.send({ embeds: [embed], components: [row] })
+    },
+};
