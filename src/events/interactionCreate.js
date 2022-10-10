fa70ded70c6f123113728cc27d5c9ec990b35e33
KieranRobson <Kieranrobson1999@gmail.com>
2022-10-10 20:10:32 +0100

    fix: Make pronoun role messages clearer

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -121,61 +121,82 @@ client.on('interactionCreate', async (interaction) => {
 		if (interaction.customId == 'HeHim') {
 			if (interaction.member.roles.cache.has('937380855402336276')) {
 				interaction.member.roles.remove('937380855402336276');
+				await interaction.reply({
+					content: 'Roles have been removed',
+					ephemeral: true,
+				});
 			} else {
 				interaction.member.roles.add('937380855402336276');
+				await interaction.reply({
+					content: 'Roles have been added',
+					ephemeral: true,
+				});
 			}
-			await interaction.reply({
-				content: 'Roles have been updated',
-				ephemeral: true,
-			});
+
 		}
 
 		if (interaction.customId == 'TheyThem') {
 			if (interaction.member.roles.cache.has('937381021022842911')) {
 				interaction.member.roles.remove('937381021022842911');
+				await interaction.reply({
+					content: 'Roles have been removed',
+					ephemeral: true,
+				});
 			} else {
 				interaction.member.roles.add('937381021022842911');
+				await interaction.reply({
+					content: 'Roles have been added',
+					ephemeral: true,
+				});
 			}
-			await interaction.reply({
-				content: 'Roles have been updated',
-				ephemeral: true,
-			});
 		}
 
 		if (interaction.customId == 'SheHer') {
 			if (interaction.member.roles.cache.has('937380980765888512')) {
 				interaction.member.roles.remove('937380980765888512');
+				await interaction.reply({
+					content: 'Roles have been removed',
+					ephemeral: true,
+				});
 			} else {
 				interaction.member.roles.add('937380980765888512');
+				await interaction.reply({
+					content: 'Roles have been added',
+					ephemeral: true,
+				});
 			}
-			await interaction.reply({
-				content: 'Roles have been updated',
-				ephemeral: true,
-			});
 		}
 
 		if (interaction.customId == 'Ask') {
 			if (interaction.member.roles.cache.has('937381232629674064')) {
 				interaction.member.roles.remove('937381232629674064');
+				await interaction.reply({
+					content: 'Roles have been removed',
+					ephemeral: true,
+				});
 			} else {
 				interaction.member.roles.add('937381232629674064');
+				await interaction.reply({
+					content: 'Roles have been added',
+					ephemeral: true,
+				});
 			}
-			await interaction.reply({
-				content: 'Roles have been updated',
-				ephemeral: true,
-			});
 		}
 
 		if (interaction.customId == 'Any') {
 			if (interaction.member.roles.cache.has('937381134269022270')) {
 				interaction.member.roles.remove('937381134269022270');
+				await interaction.reply({
+					content: 'Roles have been removed',
+					ephemeral: true,
+				});
 			} else {
 				interaction.member.roles.cache.add('937381134269022270');
+				await interaction.reply({
+					content: 'Roles have been added',
+					ephemeral: true,
+				});
 			}
-			await interaction.reply({
-				content: 'Roles have been updated',
-				ephemeral: true,
-			});
 		}
 
 		if (interaction.customId == 'projectSuggest') {
