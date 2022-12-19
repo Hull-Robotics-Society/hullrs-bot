cfb636606aae3ae5314455d59579039a5748c4af
KieranRobson <kieranrobson1999@gmail.com>
2022-12-19 13:54:30 +0000

    feat: Add lock and unlock commands

---
+++ src/slashCommands/general/lock.js
@@ -0,0 +1,35 @@
+const { Permissions } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionFlagsBits } = require('discord-api-types/v10');
+
+module.exports = {
+	...new SlashCommandBuilder()
+		.setName('lock')
+		.setDescription('Lock a channel')
+		.setDefaultMemberPermissions(
+			PermissionFlagsBits.KickMembers || PermissionFlagsBits.BanMembers
+		),
+
+	/**
+	 *
+	 * @param {Client} client
+	 * @param {CommandInteraction} interaction
+	 * @param {String[]} args
+	 */
+
+	run: async (client, interaction) => {
+		const permission = interaction.member.permissions.has(
+			Permissions.FLAGS.MANAGE_CHANNELS
+		);
+		if (!permission)
+			return interaction.reply({
+				contents: "You don't have permission to use this command",
+				ephemeral: true,
+			});
+		interaction.channel.permissionOverwrites.edit(
+			interaction.guild.roles.everyone.id,
+			{ SEND_MESSAGES: false }
+		);
+		interaction.reply('Channel has been locked.');
+	},
+};
